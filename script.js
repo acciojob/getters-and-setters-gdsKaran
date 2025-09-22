1322fb207bf5f8808a0c7fa886c8fe9acff38515
Gagandeep Singh <142520245+gdsKaran@users.noreply.github.com>
2025-09-22 19:44:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,26 +1,26 @@
 //complete this code
 class Person {
 	constructor(name, age){
-		this.name = PersonName;
-		this.age = PersonAge;
+		this.name = name;
+		this._age = age;
 	}
 	get name(){
-		return this.name;
+		return this._name;
 	}
 	set age(newAge){
-		return this.age = newAge;
+		return this._age = newAge;
 	}
 }
 
 class Student extends Person {
 	study(){
-	return `${this.name} is studying`;
+	return `${this._name} is studying`;
 	}
 }
 
 class Teacher extends Person {
 	teach(){
-		return `${this.name} is teaching`;
+		return `${this._name} is teaching`;
 	}
 }
 
